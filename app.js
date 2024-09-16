5d6173c35d124e55e1ab9835db665561d77dea31
arpitkr0 <arpitkr0.dev@gmail.com>
2024-09-16 11:57:10 +0530

    initialized

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  return res.render("index");
+});
+
+server.listen(PORT, () => console.log("Server started at port 3000!"));
---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+const socket = io();
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
+    },
+    (err) => {
+      console.log(err);
+    },
+    {
+      enableHighAccuracy: true,
+      maximumAge: 0,
+      timeout: 5000, //after every 5seconds new coordinates will be send
+    }
+  );
+}
+
+const map = L.map("map").setView([0, 0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "OpenStreetMap",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude]);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
